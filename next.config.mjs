54e408e6a6dc43f88a4a2f3c941af051b3e18d98
Vadim <vadimsvu12@mail.ru>
2024-08-15 09:58:30 +0300

    дз

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/tracks",
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/tracks",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,21 +1,22 @@
 export type StaredUserType = {
-  id: number;
+  _id: number;
   username: string;
   first_name: string;
   last_name: string;
   email: string;
   };
 export type TrackType = {
-  id: number;
-  name: string;
-  author: string;
-  release_date: string;
-  genre: string;
-  duration_in_seconds: number;
-  album: string;
-  logo: string | null;
-  track_file: string;
-  stared_user: StaredUserType[];
+    _id: number;
+    name: string;
+    author: string;
+    release_date: string;
+    genre: string;
+    duration_in_seconds: number;
+    album: string;
+    logo: string | null;
+    track_file: string;
+    stared_user: StaredUserType[];
+   
 };
 export type Props = {
   tracks: TrackType[];
